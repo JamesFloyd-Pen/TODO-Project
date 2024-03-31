29501421b297f676b367173069f97769e8413cd1
JamesFloyd-Pen <Commanderpip@gmail.com>
2024-03-31 16:34:23 -0400

    callie coding with solution
    mostly for set up work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Box, Button, ChakraProvider } from '@chakra-ui/react';
+import axios from 'axios';
 
 function App() {
+  const onClick = async () => {
+    const response = await axios.post('http://localhost:3021/name',{
+      name: "James"
+    });
+    console.log("RESPONSE: ", response);
+
+
+  };
+
+
+
   return (
     <ChakraProvider>
       <Box>Hello There</Box>
-      <Button colorScheme="red">A</Button>
+      <Button colorScheme="red" onClick={onClick}>
+        Test Button
+
+      </Button>
 
 
 
