7ddc354d8253b62f8285e6991967ddeda4f53e84
JamesFloyd-Pen <Commanderpip@gmail.com>
2024-05-11 14:41:32 -0400

    aa

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Box, Button, ChakraProvider } from '@chakra-ui/react';
+import { Box, Button, ChakraProvider, Input } from '@chakra-ui/react';
 import axios from 'axios';
 
 function App() {
+  const [name, setName] = useState('test name 3');
+
+  const onChange = (event: any) => {
+    setName(event.target.value);
+  };
+
   const onClick = async () => {
-    const response = await axios.post('http://localhost:3021/name',{
-      name: "James"
+    const response = await axios.post('http://localhost:3021/name', {
+      name,
     });
     console.log("RESPONSE: ", response);
-
-
   };
 
 
@@ -19,6 +23,8 @@ function App() {
   return (
     <ChakraProvider>
       <Box>Hello There</Box>
+      <Input onChange={onChange} placeholder = "Type in name" />
+
       <Button colorScheme="red" onClick={onClick}>
         Test Button
 
